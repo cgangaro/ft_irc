9ff44b87ae9758b352a723515987ebcd5d3aa215
Camille Gangarossa <cgangaro@z3r3p1.42lyon.fr>
2022-03-09 14:45:53 +0100

    ok

--- main.cpp
+++ main.cpp
@@ -8,24 +8,25 @@ int main (void)
 	struct sockaddr_in	sin_client;
 	socklen_t long_sock_c = sizeof(sin_client);
 
-	printf("DEBUT");
-	sock = socket(PF_INET, SOCK_STREAM, 0);
+	dprintf(1, "DEBUT\n");
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock < 0)
 		printf("Error socket");
-	printf("Socket ouverte en TCP/IP");
+	dprintf(1, "Socket ouverte en TCP/IP\n");
 	sin.sin_addr.s_addr = htonl(INADDR_ANY);
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(23); //le port
 
 	if (bind(sock, (SOCKADDR*)&sin, sizeof(sin)) < 0)
-		printf("Error bind\n");
+		dprintf(1, "Error bind\n");
 	if (listen(sock, 5) < 0) //deuxieme parametre = nombre de co pouvant etre mise en attente
-		printf("Error lsiten\n");
+		dprintf(1, "Error lsiten\n");
+	dprintf(1, "Before accept");
 	sock_client = accept(sock, (SOCKADDR*)&sin_client, &long_sock_c);
-	printf("Un client se connect avec la socket %d dde %s:%d\n", sock_client, inet_ntoa(sin_client.sin_addr), htons(sin_client.sin_port));
+	dprintf(1, "Un client se connect avec la socket %d de %s:%d\n", sock_client, inet_ntoa(sin_client.sin_addr), htons(sin_client.sin_port));
 	close(sock_client);
-	printf("Fermeture socket client");
+	dprintf(1, "Fermeture socket client");
 	close(sock);
-	printf("Fermeture socket serveur");
+	dprintf(1, "Fermeture socket serveur");
 	return (0);
 }
