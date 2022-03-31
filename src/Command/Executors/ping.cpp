c71819fb9c743935e5e144297f0743171ab8e41e
Grégory Artaud <gregory.rtd@gmail.com>
2022-03-31 16:39:58 +0200

    feature: ping

--- src/Command/Executors/ping.cpp
+++ src/Command/Executors/ping.cpp
@@ -2,8 +2,11 @@
 #include "CommunicationManager.hpp"
 
 bool Command::commandPing(Client * client, std::vector<std::string> tokens) {
-	(void)client;
-	(void)tokens;
-	std::cout << __func__ << std::endl;
+	if (tokens.size() != 2) throw Exception::ERR_NEEDMOREPARAMS(tokens[0]);
+	// :serv PONG serv :serv
+	std::string response;
+
+	response = "PONG ft_irc_server :ft_irc_server";
+	this->_communicationManager->sendMsg(client->getSocket(), buildCmdResponse(*client, response));
 	return false;
 }
